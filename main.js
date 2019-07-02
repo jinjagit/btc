28517ab47cfd530c5a111e8cd45a0964ecc7bcf1
Simon Tharby <jinjamail@gmail.com>
2019-07-02 15:38:18 +0100

    add % change since close yesterday

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 // adapted from: https://medium.com/@ssaurel/create-a-bitcoin-price-index-watcher-in-html5-f441b1e05cd1
 
-const parseJson = (json) => {
-  var usdValue = '$ ' + json["bpi"]["USD"]["rate"].split('.')[0];
-  //var gbpValue = "&pound;" + json["bpi"]["GBP"]["rate"];
-  //var euroValue = "&euro;" + json["bpi"]["EUR"]["rate"];
-
-  price.innerHTML = usdValue;
-}
-
-const getPrice = () => {
+const getPrice = (url) => {
   var xmlhttp = new XMLHttpRequest();
-  var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4  &&  this.status == 200) {
       var json = JSON.parse(this.responseText);
-      parseJson(json);
+      parse(json);
     }
   };
 
@@ -23,38 +14,40 @@ const getPrice = () => {
   xmlhttp.send();
 };
 
+const parse = (json) => {
+  if (json["bpi"]["USD"] != undefined) {
+    var usdValue = json["bpi"]["USD"]["rate"].split('.')[0];
+    //var gbpValue = "&pound;" + json["bpi"]["GBP"]["rate"];
+    //var euroValue = "&euro;" + json["bpi"]["EUR"]["rate"];
 
-const parsePrice = (json) => {
-  var obj = json["bpi"];
-  var usdValue = '$ ' + json["bpi"];
-  //var gbpValue = "&pound;" + json["bpi"]["GBP"]["rate"];
-  //var euroValue = "&euro;" + json["bpi"]["EUR"]["rate"];
+    price = parseInt(usdValue.replace(',',''));
+    priceText.innerHTML = '$' + usdValue;
+  } else {
+    var obj = json["bpi"];
 
-  console.log(usdValue);
-  console.log(json)
-
-  for (var key in obj) {
-    console.log(obj[key]);
-  }
-}
-
-const getYesterday = () => {
-  var xmlhttp = new XMLHttpRequest();
-  var url = "https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday";
-
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4  &&  this.status == 200) {
-      var json = JSON.parse(this.responseText);
-      parsePrice(json);
+    for (var key in obj) {
+      closePrice = (parseInt(obj[key]));
     }
-  };
+  }
+};
 
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
-}
+const calcPercent = () => {
+  percent = (((price / closePrice) - 1) * 100).toFixed(2);
+  percentText.innerHTML = `${percent}%`
+  if (percent >= 0) {
+    percentText.style.color = 'rgb(0, 148, 0)';
+  } else {
+    percentText.style.color = 'rgb(148, 0, 0)';
+  }
+};
 
+let priceText = document.getElementById('priceText');
+let percentText = document.getElementById('percentText');
+let closePrice = 0;
+let price = 0;
+let percent = '--%'
 
-let price = document.getElementById('price');
+getPrice("https://api.coindesk.com/v1/bpi/currentprice.json");
+getPrice("https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday");
 
-getPrice();
-getYesterday();
+setTimeout(function() { calcPercent(); }, 1000);
